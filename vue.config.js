8b817cb9b6b52380df0562613d3ba9feb4d6d482
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-07-29 21:32:28 +0300

    another test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	base: '/organic-healty-food/',
+	publicPath: '/organic-healty-food/',
 	css: {
 		loaderOptions: {
 			sass: {
